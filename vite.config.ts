87693fe184bb787c35c050558c7b2079521f2739
Hydramus <47698488+Hydramus@users.noreply.github.com>
2025-10-09 00:41:24 +0100

    setting export path in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ import type { UserConfig } from "vite";
 const dsvParsers: Record<string, ((input: string) => unknown) | undefined> = { ".csv": csvParse, ".tsv": tsvParse };
 
 export default {
-	base: "./",
+	base: "/Hakka-TTS/",
 	plugins: [
 		react(),
 		{
